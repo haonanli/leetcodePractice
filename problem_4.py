4d674932c6c826eb8899fa73860f625a42a1ec6e
lihaonan <lihaonan@youzan.com>
2020-08-02 20:07:19 +0800

    update

--- problem_4.py
+++ problem_4.py
@@ -9,38 +9,42 @@
 
 '''
 分解成分治类问题解决，求第K小的数
+注意事项：
+    1、奇偶数问题,在递归结果中处理
+    2、索引问题,第K个数在数组中的索引为k-1
+    3、边界问题,k=1与空值问题
 '''
 
 
-def func(A, B,k):
-    t=k//2
+def func(A, B, k):
+    t = k // 2
 
     # 确定最长序列与最短序列,保证A为最长序列
     if len(A) < len(B):
         A, B = B, A
 
-    print(k,t, A, B,A[0],A[1])
+    # 过程追踪
+    print(k, t, A, B, A[0], A[1])
 
-    if k==1:
-        return A[0] if len(A) % 2==1 else ((A[0]+A[1])/2)
+    # 边界条件处理
+    if k == 1:
+        return A[0] if len(A) % 2 == 1 else ((A[0] + A[1]) / 2)
 
-    if len(B)==0:
-        return A[k-1] if len(A) % 2==1 else ((A[k-1]+A[k])/2)
-
+    if len(B) == 0:
+        return A[k - 1] if len(A) % 2 == 1 else ((A[k - 1] + A[k]) / 2)
 
     # 递归
-    if A[t-1] < B[t-1]:
-        return func(A[t:], B, k-t)
+    if A[t - 1] < B[t - 1]:
+        return func(A[t:], B, k - t)
     else:
-        return func(A, B[t:], k-t)
+        return func(A, B[t:], k - t)
 
 
 if __name__ == '__main__':
-    A = [4,5,6]
-    B = [3, 4, 5,5,9]
-    # print(1 / 2)
+    A = [4, 5, 6]
+    B = [3, 4, 5, 5, 9]
     len_A = len(A)
     len_B = len(B)
-    k = (len_A + len_B)//2
+    k = (len_A + len_B) // 2
     medium = func(A, B, k)
     print("medium is {}".format(medium))
